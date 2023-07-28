71548630f74e20a2571d947b3e1a5e47122660ff
Huynh <huynhlam56@gmail.com>
2023-07-28 16:41:16 -0700

    frontend-finished-modals

--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -1,47 +1,46 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector, useStore } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useModal } from "../../context/Modal";
 import * as sessionActions from "../../store/session";
 import "./SignupForm.css";
 
-
-function SignupFormPage() {
+function SignupFormModal() {
   const dispatch = useDispatch();
-  const sessionUser = useSelector((state) => state.session.user)
-  const [username, setUsername] = useState('');
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [errors, setErrors] = useState('')
-
-  console.log(sessionUser)
-  if (sessionUser) return <Redirect to="/" />;
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errors, setErrors] = useState({});
+  const { closeModal } = useModal();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(password === confirmPassword) {
-      setErrors({})
-      return dispatch(sessionActions.signup({
-        email,
-        username,
-        firstName,
-        lastName,
-        password
-      })
-      ).catch(async(response) => {
-        const data = await response.json();
-        console.log(data)
-        if (data && data.errors) {
-          setErrors(data.errors)
-        }
-      })
+    if (password === confirmPassword) {
+      setErrors({});
+      return dispatch(
+        sessionActions.signup({
+          email,
+          username,
+          firstName,
+          lastName,
+          password,
+        })
+      )
+        .then(closeModal)
+        .catch(async (res) => {
+          const data = await res.json();
+          if (data && data.errors) {
+            setErrors(data.errors);
+          }
+        });
     }
     return setErrors({
       confirmPassword: "Confirm Password field must be the same as the Password field"
-    })
-  }
+    });
+  };
+
   return (
     <>
       <h1>Sign Up</h1>
@@ -105,11 +104,13 @@ function SignupFormPage() {
             required
           />
         </label>
-        {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+        {errors.confirmPassword && (
+          <p>{errors.confirmPassword}</p>
+        )}
         <button type="submit">Sign Up</button>
       </form>
     </>
   );
 }
 
-export default SignupFormPage;
+export default SignupFormModal;
